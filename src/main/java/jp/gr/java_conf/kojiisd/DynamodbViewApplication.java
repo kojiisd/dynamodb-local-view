31171d6e1beb7076b2c36a4702a9cd9497395653
kojiisd <kishida58@gmail.com>
2017-01-04 18:27:31 +0630

    initial commit

---
+++ src/main/java/jp/gr/java_conf/kojiisd/DynamodbViewApplication.java
@@ -0,0 +1,12 @@
+package jp.gr.java_conf.kojiisd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbViewApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/gr/java_conf/kojiisd/DynamodbViewApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.gr.java_conf.kojiisd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynamodbViewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
