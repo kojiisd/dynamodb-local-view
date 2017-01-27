18974c06764e6dcd50a8ce0e921371268ff6bbb8
Koji Ishida <kishida58@gmail.com>
2017-01-27 12:52:13 +0630

    enable accessing to DynamoDB with not default setting (localhost:8080)

--- src/main/resources/static/js/dynamodb-viewer.js
+++ src/main/resources/static/js/dynamodb-viewer.js
@@ -2,29 +2,36 @@ var dynamoDbViewer = angular.module('DynamoDbViewer', ['ngTable', 'angular-loadi
 
 dynamoDbViewer.controller("listTables",
     function listTables($scope, $http, ngTableParams, cfpLoadingBar) {
-    $scope.tableParams = new ngTableParams(
-        { page: 1,
-          count: 10 },
-        { counts: [],
-          total: 1,
-          dataset: $scope.tables });
+    $scope.init = function(host, port) {
+        $scope.tableParams = new ngTableParams(
+            { page: 1,
+              count: 10 },
+            { counts: [],
+              total: 1,
+              dataset: $scope.tables });
 
-    cfpLoadingBar.start();
-    $http.get('http://localhost:8080/api/list-tables')
-        .then(function(response) {
-            //First function handles success
-            $scope.tables = response.data;
-            cfpLoadingBar.complete();
-        }, function(response) {
-            cfpLoadingBar.complete();
-            //Second function handles error
-            alert("Can not connect to DynamoDB Local. Please check connection and try again.");
-        });
+        var tmpHost = (host == "" || host == null) ? "localhost" : host;
+        var tmpPort = (port == "" || port == null) ? "8080" : port;
+
+        var url = "http://" + tmpHost + ":" + tmpPort + "/api/list-tables"
+        cfpLoadingBar.start();
+        $http.get(url)
+            .then(function(response) {
+                //First function handles success
+                $scope.tables = response.data;
+                cfpLoadingBar.complete();
+            }, function(response) {
+                cfpLoadingBar.complete();
+                //Second function handles error
+                alert("Can not connect to DynamoDB Local. Please check connection and try again.");
+            });
+        };
     });
+
 
 dynamoDbViewer.controller("inquiryTable",
     function inquiryTable($scope, $http, ngTableParams, cfpLoadingBar){
-    $scope.init = function(tableName) {
+    $scope.init = function(tableName, host, port) {
         $scope.tableName = tableName;
         $scope.tableParams = new ngTableParams(
             { page: 1,
@@ -33,8 +40,12 @@ dynamoDbViewer.controller("inquiryTable",
               total: 1,
               dataset: $scope.tables });
 
+        var tmpHost = (host == "" || host == null) ? "localhost" : host;
+        var tmpPort = (port == "" || port == null) ? "8080" : port;
+
+        var url = "http://" + tmpHost + ":" + tmpPort + "/api/inquiry-table/"
         cfpLoadingBar.start();
-        $http.get('http://localhost:8080/api/inquiry-table/' + $scope.tableName)
+        $http.get(url + $scope.tableName)
             .then(function(response) {
                 //First function handles success
                 $scope.datas = response.data;
